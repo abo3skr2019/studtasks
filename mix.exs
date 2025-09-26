6c9a0527a5dac9343c30d031ee4c5f7fc8f18a05
abo3skr2019 <68832286+abo3skr2019@users.noreply.github.com>
2025-09-26 15:08:48 +0300

    more more more moer

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule Studtasks.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:tidewave, "~> 0.5", only: [:dev]},
       {:igniter, "~> 0.6", only: [:dev, :test]},
       {:argon2_elixir, "~> 4.0"},
       {:phoenix, "~> 1.8.1"},
